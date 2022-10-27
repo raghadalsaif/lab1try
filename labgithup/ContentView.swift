69665a2c815f8802e12bf54e3d282aed97eb983b
raghad <raghad.k.alsaif@gmail.com>
2022-10-27 16:50:47 +0300

    aaaa

--- labgithup/ContentView.swift
+++ labgithup/ContentView.swift
@@ -5,17 +5,33 @@
 //  Created by raghad khalid alsaif on 02/04/1444 AH.
 //
 
+
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
+        VStack(alignment: .center) {
+            Image( "Image")
+                .resizable()
+                .frame(width: 250,height: 250)
+                .cornerRadius(140)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+                .clipShape(Circle())
+                .overlay { Circle().stroke(.gray, lineWidth: 3)}
+                .shadow(radius: 5)
+            Spacer()
+            
+            Text("raghad alsaif")
+                .font(.largeTitle)
+                .foregroundColor(Color.gray)
+                
+            Spacer()
+             .frame(height: 20)
+            Text("i’m just writing for the purpose of writing, do you still read I’m sorry for you, there’s nothing to read it just nothing. “you’re amazing”")
+                .multilineTextAlignment(.center)
+           
         }
-        .padding()
+        .padding(100)
     }
 }
 
