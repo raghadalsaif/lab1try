30c7bb6f872b8e7ce0158d69bcb32496de466e53
Malak <malakhulimi@gmail.com>
2022-10-27 16:59:26 +0300

    add  screen

---
+++ labgithup/malakhulimi.swift
@@ -0,0 +1,20 @@
+//
+//  malakhulimi.swift
+//  labgithup
+//
+//  Created by Malak  on 02/04/1444 AH.
+//
+
+import SwiftUI
+
+struct malakhulimi: View {
+    var body: some View {
+        Text("kkkkkkkk")
+    }
+}
+
+struct malakhulimi_Previews: PreviewProvider {
+    static var previews: some View {
+        malakhulimi()
+    }
+}
