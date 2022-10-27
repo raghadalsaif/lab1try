85af8345b63346858f71a7fe0b7453d75e974890
raghad <raghad.k.alsaif@gmail.com>
2022-10-27 16:40:54 +0300

    Initial Commit

---
+++ labgithup/labgithupApp.swift
@@ -0,0 +1,17 @@
+//
+//  labgithupApp.swift
+//  labgithup
+//
+//  Created by raghad khalid alsaif on 02/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct labgithupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
